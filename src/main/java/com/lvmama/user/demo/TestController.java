3fed34589effecf0f19a3483e5e6899e12001778
leriou <yange1876@hotmail.com>
2018-01-22 01:06:56 +0800

    init it

---
+++ src/main/java/com/lvmama/user/demo/TestController.java
@@ -0,0 +1,14 @@
+package com.lvmama.user.demo;
+
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    String index(){
+        return "test index";
+    }
+}
