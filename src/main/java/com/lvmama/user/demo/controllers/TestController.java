94feab70467901176b204945c6813e6262f3a035
leriou <yange1876@hotmail.com>
2018-01-26 00:23:14 +0800

    修改测试路径

--- src/main/java/com/lvmama/user/demo/controllers/TestController.java
+++ src/main/java/com/lvmama/user/demo/controllers/TestController.java
@@ -8,12 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
 public class TestController {
 
     @Autowired
     public StringRedisTemplate redisTemplate;
 
-    @RequestMapping("/test")
+    @RequestMapping("/index")
     public String index(){
         return "test index";
     }
@@ -27,7 +28,6 @@ public class TestController {
 
     @RequestMapping("/redisGet")
     public String redisGet(){
-
         ValueOperations<String,String> ops =  redisTemplate.opsForValue();
         return ops.get("spring10086");
     }
